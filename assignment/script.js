e3039a92169051563af324f651003406315576d0
aarthi-ldp <135806342+aarthi-ldp@users.noreply.github.com>
2023-06-16 11:56:27 +0530

    async files added

---
+++ assignment/script.js
@@ -0,0 +1,53 @@
+
+function getData(uId) {
+    return new Promise((resolve, reject)=>{
+    setTimeout(() => {
+    console.log("Fetched the data!");
+    resolve ("skc@gmail.com");
+    }, 4000);
+    });
+}
+    
+
+    async function asyncstatus(){
+    try{
+        console.log("start");
+    var email = await(getData("skc"));
+    console.log("Email id of the user id is: " + email);
+    console.log("end");
+    }
+
+catch(err){
+    console.log(err)
+}
+    }
+asyncstatus()
+// function getData(uId) {
+//     return new Promise((resolve, reject) => {
+//       setTimeout(() => {
+//         console.log("Fetched the data!");
+//         resolve("skc@gmail.com");
+//       }, 4000);
+//     });
+//   }
+  
+//   async function asyncstatus() {
+//     try {
+//       console.log("start");
+//       var email = await getData("skc");
+//       console.log("Email id of the user id is: " + email);
+//       console.log("end");
+//     } catch (err) {
+//       console.log(err);
+//     }
+//   }
+  
+//   asyncstatus();
+ 
+  
+  
+  
+  
+  
+  
+  
