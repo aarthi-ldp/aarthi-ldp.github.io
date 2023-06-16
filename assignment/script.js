6bead2ac5e16e00baf59c5afffb249c9e8edcc62
aarthi-ldp <135806342+aarthi-ldp@users.noreply.github.com>
2023-06-16 11:59:19 +0530

    Update script.js

--- assignment/script.js
+++ assignment/script.js
@@ -22,28 +22,7 @@ catch(err){
 }
     }
 asyncstatus()
-// function getData(uId) {
-//     return new Promise((resolve, reject) => {
-//       setTimeout(() => {
-//         console.log("Fetched the data!");
-//         resolve("skc@gmail.com");
-//       }, 4000);
-//     });
-//   }
-  
-//   async function asyncstatus() {
-//     try {
-//       console.log("start");
-//       var email = await getData("skc");
-//       console.log("Email id of the user id is: " + email);
-//       console.log("end");
-//     } catch (err) {
-//       console.log(err);
-//     }
-//   }
-  
-//   asyncstatus();
- 
+
   
   
   
