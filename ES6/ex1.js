5707d9a9316a8a01834589fb70ff51c5f2280329
aarthi-ldp <135806342+aarthi-ldp@users.noreply.github.com>
2023-06-16 16:17:45 +0530

    ES6

---
+++ ES6/ex1.js
@@ -0,0 +1,8 @@
+// Refactor the following function into a one-liner:
+// const printName = (name) => {
+//                      return “Hi” + name;
+//            }
+
+
+
+const printName = (newname) =>"Hi" +newname;
