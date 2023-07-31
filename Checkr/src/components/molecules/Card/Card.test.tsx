0b233e5784c68d3295fa0c423dbc9d998bedda4a
aarthi-ldp <135806342+aarthi-ldp@users.noreply.github.com>
2023-07-31 13:24:14 +0530

    Added Card Testcases

--- Checkr/src/components/molecules/Card/Card.test.tsx
+++ Checkr/src/components/molecules/Card/Card.test.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
-import Iconography from '../Iconography';
-import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import { render, waitFor, screen } from '@testing-library/react';
+import CardArray from '../../../components/molecules/Card/index';
+import '@testing-library/jest-dom/extend-expect';
+import AccountBoxIcon from '@mui/icons-material/AccountBox'; // Import the necessary icons here
 import MailOutlineIcon from '@mui/icons-material/MailOutline';
-import PhoneIcon from '@mui/icons-material/Phone';
 import EventNoteIcon from '@mui/icons-material/EventNote';
+import PhoneIcon from '@mui/icons-material/Phone';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 import ShieldIcon from '@mui/icons-material/Shield';
 import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
-import { Box } from '@mui/material';
 
-const Card = [
-  {  id:1, iconComponent: AccountBoxIcon, iconAlt: 'Account Icon', text1: 'Name', text2: 'John Smith' },
+jest.mock('../Iconography', () => () => <div data-testid="mock-iconography" />);
+
+describe('CardArray', () => {
+  const cards = [
+    {  id:1, iconComponent: AccountBoxIcon, iconAlt: 'Account Icon', text1: 'Name', text2: 'John Smith' },
   {  id:2, iconComponent: MailOutlineIcon, iconAlt: 'Mail Icon', text1: 'Email', text2: 'John.smith@checkr.com' },
   {  id:3, iconComponent: EventNoteIcon, iconAlt: 'Event Icon', text1: 'DOB', text2: '1990-09-10(26)' },
   {  id:4, iconComponent: PhoneIcon, iconAlt: 'Phone Icon', text1: 'Phone', text2: '(555) 555-5555' },
@@ -18,27 +22,19 @@ const Card = [
   {  id:6, iconComponent: ShieldIcon, iconAlt: 'Shield Icon', text1: 'Social Security', text2: 'XXX-XX-6789' },
   {  id:7, iconComponent: EventNoteIcon, iconAlt: 'Drivers License Icon', text1: 'Drivers License', text2: 'FTEST1111(CA)' },
   {  id:8, iconComponent: CalendarTodayIcon, iconAlt: 'Calendar Icon', text1: 'Created At', text2: 'Nov 29, 2016 11:05:57 AM' },
-];
+   
+  ];
 
-const CardArray = () => {
-  return (
-    <Box data-testid="card-element" display ="flex" flexWrap="wrap" gap={2} >
+  it('should render all cards with correct data', () => {
+    const { getByTestId, getByText, debug } = render(<CardArray />);
     
-      {Card.map((card) => (
-          <Box key={card.id} sx={{ flexBasis: '30%',border: '1px solid #ccc', padding: '10px', marginBottom: '10px', boxShadow: '0px 2px 5px rgba(0, 0, 0, 0.1)' }}>
-        <Iconography
+   
+    const cardElements = getByTestId('card-element');
+    expect(cardElements.children.length).toBe(cards.length);
 
-          key={card.id}
-          iconComponent={card.iconComponent}
-          iconAlt={card.iconAlt}
-          text1={card.text1}
-          text2={card.text2}
-        />
-        </Box>
-      ))}
     
-    </Box>
-  );
-};
+    
+    
+  });
 
-export default CardArray;
+});
